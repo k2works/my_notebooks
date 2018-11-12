97989bad2dbb4a5d9e8365912aaed1abf6dc03ed
k2works <kakimomokuri@gmail.com>
2018-11-12 14:41:09 +0900

    仮実装

--- source/programing_exercises/python/20181112/main_test.py
+++ source/programing_exercises/python/20181112/main_test.py
@@ -1,8 +1,12 @@
 import unittest
 
+
+def execute():
+  pass
+
 class MainTest(unittest.TestCase):
-  def test_初めの失敗するテスト(self):
-    self.assertTrue(False)
+  def test_結果を１００回出力する(self):
+    self.assertTrue("1", execute())
 
 if __name__ == "__main__":
     unittest.main()
