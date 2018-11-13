2ab21e683099964df6ce157e5fb6cabe550f49e0
k2works <kakimomokuri@gmail.com>
2018-11-13 11:55:14 +0900

    明白な実装
    関数
    if文
    リスト

--- source/programing_exercises/python/20181113/main_test.py
+++ source/programing_exercises/python/20181113/main_test.py
@@ -6,7 +6,11 @@ def exectue():
     n = 100
 
     while n != 0:
-        print(n)
+        if n % 3 == 0:
+            print("Fizz")
+        else:
+            print(n)
+        
         n = n - 1
 
 
@@ -19,6 +23,13 @@ class MainTest(unittest.TestCase):
 
         self.assertEqual(lines[0], "100")
         self.assertEqual(lines[99], "1")
+
+    def test_3の倍数のときは数の代わりにFizzをプリントする(self):
+        with captured_stdout() as stdout:
+            exectue()
+            lines = stdout.getvalue().splitlines()
+
+        self.assertEqual(lines[97], "Fizz")
 
 
 if __name__ == "__main__":
